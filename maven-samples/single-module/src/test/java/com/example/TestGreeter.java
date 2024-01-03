1c8a338b378b88f17b607fe26eaa6aebccb2ebe8
Sumit Patkulkar <117511730+sumitpatkulkar@users.noreply.github.com>
2024-01-03 20:55:36 +0530

    Update TestGreeter.java method removed

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -24,11 +24,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
-   @Test
-  public void custMethod() {
-   assertThat(true));
-  }
-
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
